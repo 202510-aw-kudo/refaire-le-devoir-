d0d8b7aea9d3e4a596e4e4565d1e1a2aa91f495d
202510-aw-kudo <202510_aw_kudo@x-ide.sakura.ne.jp>
2026-02-05 14:58:50 +0900

    le debut

---
+++ src/main/java/com/example/refaire/le/devoir/RefaireLeDevoirApplication.java
@@ -0,0 +1,13 @@
+package com.example.refaire.le.devoir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefaireLeDevoirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefaireLeDevoirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/refaire/le/devoir/RefaireLeDevoirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.refaire.le.devoir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefaireLeDevoirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
