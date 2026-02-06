7326de647c8de14a2a7f58dffc5eb8d70cbec5b7
202510-aw-kudo <202510_aw_kudo@x-ide.sakura.ne.jp>
2026-02-06 10:19:17 +0900

    a revoir

--- src/main/java/com/example/refaire/le/devoir/controller/TodoController.java
+++ src/main/java/com/example/refaire/le/devoir/controller/TodoController.java
@@ -1,6 +1,7 @@
-package com.example.refaire.le.devoir.controller;
+﻿package com.example.refaire.le.devoir.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -13,15 +14,23 @@ public class TodoController {
     return "todo/list";
   }
 
-  // Affiche le formulaire de création d'un todo.
+  // Affiche le formulaire de creation d'un todo.
   @GetMapping("/todos/new")
   public String newForm() {
     return "todo/form";
   }
 
-  // Affiche le détail d'un todo par identifiant.
+  // Affiche le detail d'un todo par identifiant.
   @GetMapping("/todos/{id}")
-  public String detail(@PathVariable("id") Long id) {
+  public String detail(@PathVariable("id") Long id, Model model) {
+    model.addAttribute("todoId", id);
     return "todo/detail";
+  }
+
+  // Affiche le formulaire d'edition d'un todo.
+  @GetMapping("/todos/{id}/edit")
+  public String edit(@PathVariable("id") Long id, Model model) {
+    model.addAttribute("todoId", id);
+    return "todo/edit";
   }
 }
